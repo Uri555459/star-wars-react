4a33a58dc4224dbab662c69e468b5b6ff959cdb2
Uri <uri555459@gmail.com>
2022-04-05 08:53:17 +0300

    Added project github

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,13 @@
+import PeoplePage from '@containers/PeoplePage'
+
+import styles from './App.module.css'
+
+const App = () => {
+  return (
+    <>
+      <PeoplePage />
+    </>
+  )
+}
+
+export default App
